3e22cc14d74251c4adf19474e5d6c6b716cad831
Daart <david.picart@outlook.fr>
2022-12-09 21:53:04 +0100

    ex08

--- src/fr/daart/Main.java
+++ src/fr/daart/Main.java
@@ -7,6 +7,7 @@ import fr.daart.ex04.AoC04;
 import fr.daart.ex05.AoC05;
 import fr.daart.ex06.AoC06;
 import fr.daart.ex07.AoC07;
+import fr.daart.ex08.AoC08;
 
 public class Main {
     public static void main(String[] args) {
@@ -18,6 +19,7 @@ public class Main {
         new AoC05().run();
         new AoC06().run();
         new AoC07().run();
+        new AoC08().run();
 
     }
 }
