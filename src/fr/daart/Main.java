a4be5c7cfae8da7f694f1ca31a390933dbe6ee6c
Daart <david.picart@outlook.fr>
2022-12-09 21:51:32 +0100

    ex04

--- src/fr/daart/Main.java
+++ src/fr/daart/Main.java
@@ -3,6 +3,7 @@ package fr.daart;
 import fr.daart.ex01.AoC01;
 import fr.daart.ex02.AoC02;
 import fr.daart.ex03.AoC03;
+import fr.daart.ex04.AoC04;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,6 +11,7 @@ public class Main {
         new AoC01().run();
         new AoC02().run();
         new AoC03().run();
+        new AoC04().run();
 
     }
 }
---
+++ src/fr/daart/ex04/AoC04.java
@@ -0,0 +1,71 @@
+package fr.daart.ex04;
+
+import fr.daart.AoC2022;
+
+import java.util.Arrays;
+
+public class AoC04 extends AoC2022 {
+
+    @Override
+    public int getDay() {
+        return 4;
+    }
+
+    @Override
+    public void part1() {
+        var input = readInput("input.txt");
+        var inputSize = input.size();
+        int overlapped = 0;
+        for (int i = 0; i < inputSize; i++) {
+            var line = input.get(i);
+            var ranges = line.split(",");
+            boolean overlap = doesOneRangeContainsTheOther(ranges[0], ranges[1]);
+            //System.out.println(line + " : " + overlap);
+            if (overlap) {
+                overlapped++;
+            }
+        }
+        System.out.println("Part 1: " + overlapped);
+    }
+
+    @Override
+    public void part2() {
+        var input = readInput("input.txt");
+        var inputSize = input.size();
+        int overlapped = 0;
+        for (int i = 0; i < inputSize; i++) {
+            var line = input.get(i);
+            var ranges = line.split(",");
+            var areOverlapping = areOverlapped(ranges[0], ranges[1]);
+            //System.out.println(line + " overlapQuantity = " + areOverlapping);
+            if (areOverlapping) {
+                overlapped++;
+            }
+        }
+        System.out.println("Part 2: " + overlapped);
+    }
+
+    private boolean areOverlapped(String range1String, String range2String) {
+
+        int[] range1 = Arrays.stream(range1String.split("-")).mapToInt(Integer::parseInt).sorted().toArray();
+        int[] range2 = Arrays.stream(range2String.split("-")).mapToInt(Integer::parseInt).sorted().toArray();
+
+        return (range1[0] >= range2[0] && range1[1] <= range2[1])
+                || (range1[1] >= range2[0] && range1[1] <= range2[1])
+                || (range2[0] >= range1[0] && range2[1] <= range1[1])
+                || (range2[1] >= range1[0] && range2[1] <= range1[1]);
+    }
+
+    private boolean doesOneRangeContainsTheOther(String range1String, String range2String) {
+        int[] range1 = Arrays.stream(range1String.split("-")).mapToInt(Integer::parseInt).sorted().toArray();
+        int[] range2 = Arrays.stream(range2String.split("-")).mapToInt(Integer::parseInt).sorted().toArray();
+        if (range1[0] <= range2[0] && range1[1] >= range2[1]) {
+            return true;
+        } else if (range1[0] >= range2[0] && range1[1] <= range2[1]) {
+            return true;
+        }
+        return false;
+    }
+
+
+}
